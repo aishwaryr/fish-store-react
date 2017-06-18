54ee175a43b9ae137367b1b13145561ebfda63ef
aishwaryr <araj713@gmail.com>
2017-06-18 22:52:58 +0530

    Init push - hellow orld working

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+    render () {
+        return (
+            <div className="catch-of-the-day">
+                // <div className="menu">
+                //     <Header />
+                // </div>
+                // <Order />
+                // <Inventory />
+            </div>
+        )
+    }
+}
