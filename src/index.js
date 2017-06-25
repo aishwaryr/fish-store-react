7ca13d6bb645e3d058057bf314aae8891c053df4
aishwaryr <araj713@gmail.com>
2017-06-25 13:47:02 +0530

    somewhre on step 6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import './css/style.css';
+import App from './components/App.jsx';
 
-import StorePicker from './components/StorePicker';
+import StorePicker from './components/StorePicker.jsx';
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
