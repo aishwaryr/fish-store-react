fa46c351cfb696e65d9bccd90e4ae3a4ece237e9
aishwaryr <araj713@gmail.com>
2017-06-25 23:55:53 +0530

    Upto vid 11: Added event on input submit

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,24 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this);
+    // }
+
+    goToStore(event) {
+        event.preventDefault();
+        console.log('You changed the URL');
+        // first grab text from box
+        console.log(this);
+        // second transition URL from / to /store/:storeId
+    }
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+                <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
                 <button type="submit">Visit Store</button>
             </form>
         )
