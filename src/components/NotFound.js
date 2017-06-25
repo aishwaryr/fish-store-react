464ec9420d0f091d37e8a7a61c03a7c6d593fa22
aishwaryr <araj713@gmail.com>
2017-06-25 18:35:58 +0530

    Done upto vid-10: helper funcs

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// This is a stateless function, it doesnt extends React.Component
+// cause it doesn't have any other functionality than to just show some HTML.
+
+const NotFound = (props) => {
+    return (
+        <h2>Not Found!111!!</h2>
+    )
+}
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss} from 'react-router';
+
 import './css/style.css';
 import App from './components/App';
-
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App/>, document.querySelector('#main'));
+// A stateless func for routing
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern="/" component={StorePicker}></Match>
+                <Match pattern="/store/:storeId" component={App}></Match>
+                <Miss component={NotFound}></Miss>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+// Changed Render-> App to Root so it will render comp based on url
+render(<Root/>, document.querySelector('#main'));
